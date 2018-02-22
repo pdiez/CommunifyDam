ab0f1fdabc0953f460350314e584c9bcf35c64c8
Oscar Uceda Gutierrez <oscar.uceda1@gmail.com>
2018-02-22 13:15:37 +0100

    Oscar wizard terminado

--- app/src/main/java/com/communifydam/app/communify/FragmentAddComunidad_2.java
+++ app/src/main/java/com/communifydam/app/communify/FragmentAddComunidad_2.java
@@ -19,15 +19,23 @@ public class FragmentAddComunidad_2 extends Fragment {
 
     private EditText et_descripcion_comunidad;
     private GrabarAnuncio ga;
+    private Button crear_comunidad;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.add_comunidad_step_2, container, false);
         ViewPager vp=(ViewPager) this.getParentFragment().getView().findViewById(R.id.dgPagerComunidad);
         AdaptadorPagerAddComunidad mi_vp=(AdaptadorPagerAddComunidad)vp.getAdapter();
+        et_descripcion_comunidad=(EditText)v.findViewById(R.id.et_descripcion_comunidad);
+        mi_vp.setEt_descripcion_comunidad(et_descripcion_comunidad);
         ga=(GrabarAnuncio)mi_vp;//Está por ver
-
-
+        crear_comunidad = (Button)v.findViewById(R.id.crearComunidad);
+        crear_comunidad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ga.grabar("creado");
+            }
+        });
         return v;
     }
 
